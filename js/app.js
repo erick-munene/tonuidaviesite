839895b8ceb336ae257a64e58d94bf5c48a26649
Davies Tonui <tonuidavies@gmail.com>
2016-06-25 21:41:55 +0300

    js added for footer

--- js/app.js
+++ js/app.js
@@ -41,3 +41,15 @@ $(document).ready(function() {
         });
     } 
 });
+
+
+//footer
+
+$(function() {
+  $('.js-nav a, .js-connect').click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
+  });
+});
